41f25976815be0b55cad5c488609c6272ac8d674
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-10-29 22:30:37 +0800

    Create ci_i.R
    Find both the lower and upper bounds of a parameter

---
+++ R/ci_i.R
@@ -0,0 +1,40 @@
+#'@title Find the lower bound and upper bound for one parameter
+#'
+#'@description Find the lower and upper bound for one parameter.
+#'
+#'@details
+#'
+#' Currently supports \code{lavaan} output only.
+#'
+#'@return
+#' The requested bounds
+#' Can return the optimization history as an attribute.
+#'
+#' @param i The position of the target parameters.
+#' @param ... Parameters to be passed to \code{ci_bound_i}.
+#' 
+#'@examples
+#' library(lavaan)
+#' data(cfa_two_factors)
+#' mod <-
+#' "
+#' f1 =~ x1 + x2 + a*x3
+#' f2 =~ x4 + a*x5 + equal('f1=~x2')*x6
+#' f1 ~~ 0*f2
+#' asq := a^2
+#' "
+#' fit <- sem(mod, cfa_two_factors)
+#'@export
+
+ci_i <- function(i, ...) {
+    lb <- ci_bound_i(i, which = "lbound", ...)
+    ub <- ci_bound_i(i, which = "ubound", ...)
+    out <- c(lb, ub)
+    if (!is.null(attr(lb, "history"))) {
+        attr(out, "lb_history") <- attr(lb, "history")
+      }
+    if (!is.null(attr(ub, "history"))) {
+        attr(out, "ub_history") <- attr(ub, "history")
+      }
+    out
+  }
