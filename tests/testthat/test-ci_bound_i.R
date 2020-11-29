e1f1375a274e1a237a39bb8dce5408a2a9185b39
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-11-29 17:54:55 +0800

    Update test-ci_bound_i.R
    Add system.time

--- tests/testthat/test-ci_bound_i.R
+++ tests/testthat/test-ci_bound_i.R
@@ -14,10 +14,10 @@ fit_med <- lavaan::sem(mod, simple_med, fixed.x = FALSE)
 
 fn_constr0 <- set_constraint(fit_med)
 
-out1l <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound")
-out1u <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound")
-out2l <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound")
-out2u <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound")
+system.time(out1l <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound"))
+system.time(out1u <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound"))
+system.time(out2l <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound"))
+system.time(out2u <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound"))
 
 
 library(OpenMx)
