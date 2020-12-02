d8e2d20d4547a3c69a30ac22e58b1c05e1a31ef8
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-12-02 19:58:25 +0800

    Add enforce_eq_by_label to expand a vector of reduced parameters to the full vector of parameters

---
+++ R/enforce_eq_by_label.R
@@ -0,0 +1,17 @@
+enforce_eq_by_label <- function(i_depend, p_table) {
+    j_no_label <- p_table$label == ""
+    p_table[j_no_label, "label"] <- 
+        paste0("@@", p_table[j_no_label, "id"], "@@")
+    labels <- p_table[p_table$free > 0,"label"]
+    labels <- unique(labels[labels != ""])
+    fct <- function(x) {
+        which(p_table$label == p_table$label[x])
+      }
+    fct2 <- function(x) {
+        rep(x, sum(p_table$label == p_table$label[x]))
+      }
+    out <- lapply(i_depend, fct)
+    out2 <- lapply(i_depend, fct2)
+    list(i_exp_target = unlist(out),
+         i_exp_source = unlist(out2))
+  }
---
+++ tests/testthat/test-enforce_eq_by_label.R
@@ -0,0 +1,101 @@
+# WARNING!
+# WIP. Not ready. semlbci not yet works for the CFA model.
+
+library(testthat)
+library(semlbci)
+
+context("Check semlbci: With equality constraints")
+
+dat <- cfa_two_factors
+mod <- 
+"
+f1 =~ x1 + b*x2 + c*x3
+f2 =~ x4 + c*x5 + x6
+"
+fit <- lavaan::sem(mod, cfa_two_factors, fixed.x = FALSE)
+p_table <- lavaan::parameterTable(fit)
+options(widht = 1802)
+p_table
+
+out1 <- semlbci:::enforce_eq_by_label(c(2, 3), p_table)
+out2 <- semlbci:::enforce_eq_by_label(c(2, 5), p_table)
+out3 <- semlbci:::enforce_eq_by_label(c(1, 2, 5), p_table)
+out4 <- semlbci:::enforce_eq_by_label(c(1, 2), p_table)
+out5 <- semlbci:::enforce_eq_by_label(c(1, 4, 6), p_table)
+
+
+test_that("Test 1a", {
+    expect_equal(
+      out1$i_exp_target,
+      c(2, 3, 5)  
+    )
+  })
+
+test_that("Test 1b", {
+    expect_equal(
+      out1$i_exp_source,
+      c(2, 3, 3)  
+    )
+  })
+
+
+test_that("Test 2a", {
+    expect_equal(
+      out2$i_exp_target,
+      c(2, 3, 5)  
+    )
+  })
+
+test_that("Test 2b", {
+    expect_equal(
+      out2$i_exp_source,
+      c(2, 5, 5)  
+    )
+  })
+
+
+
+test_that("Test 3b", {
+    expect_equal(
+      out3$i_exp_target,
+      c(1, 2, 3, 5)  
+    )
+  })
+
+test_that("Test 3b", {
+    expect_equal(
+      out3$i_exp_source,
+      c(1, 2, 5, 5)  
+    )
+  })
+
+
+
+test_that("Test 4b", {
+    expect_equal(
+      out4$i_exp_target,
+      c(1, 2)  
+    )
+  })
+
+test_that("Test 4b", {
+    expect_equal(
+      out4$i_exp_source,
+      c(1, 2)  
+    )
+  })
+
+
+test_that("Test 5b", {
+    expect_equal(
+      out5$i_exp_target,
+      c(1, 4, 6)  
+    )
+  })
+
+test_that("Test 5b", {
+    expect_equal(
+      out5$i_exp_source,
+      c(1, 4, 6)  
+    )
+  })
