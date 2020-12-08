6076fd7783257aa995ecdaa8fe1b7fbf5f7ea687
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-12-08 10:18:48 +0800

    Add test datasets with estimates near the attainable bounds

--- R/data.R
+++ R/data.R
@@ -1,8 +1,15 @@
-#'Sample dataset for testing
-#' 
 #' Generated from a two-factor model with six variables, n 500
 #' 
 "cfa_two_factors"
 
 #' Generated from a simple mediation model, n 500
+#' 
 "simple_med"
+
+#' Generated from a regression model, with one correlation close to one.
+#' 
+"reg_cor_near_one"
+
+#' Generated from a two-factor model, with one error variacne close to zero.
+#' 
+"cfa_evar_near_zero"
