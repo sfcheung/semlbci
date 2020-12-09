876291ebb4eab1dac7b02d843db4722239da1850
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-12-09 22:46:40 +0800

    Update test-ci_bound_i_standardized.R
    Change the tolerance to speed up the tests

--- tests/testthat/test-ci_bound_i_standardized.R
+++ tests/testthat/test-ci_bound_i_standardized.R
@@ -17,10 +17,10 @@ fn_constr0 <- set_constraint(fit_med)
 # opts0 <- list(print_level = 3)
 opts0 <- list()
 opts0 <- list(ftol_abs = 1e-7,
-              ftol_rel = 1e-7,
-              xtol_abs = 1e-7,
-              xtol_rel = 1e-7,
-              tol_constraints_eq = 1e-7
+              ftol_rel = 1e-7
+              # xtol_abs = 1e-3,
+              # xtol_rel = 1e-3,
+              # tol_constraints_eq = 1e-3
               )
 system.time(out1l <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound", standardized = TRUE, opts = opts0))
 system.time(out1u <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound", standardized = TRUE, opts = opts0))
