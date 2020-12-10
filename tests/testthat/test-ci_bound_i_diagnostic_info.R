2117f2a3f58e022633b73ba7365ca41494d7210d
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-12-10 22:00:14 +0800

    Remove standardized  = TRUE cases from test-ci_bound_i_diagnostic_info

--- tests/testthat/test-ci_bound_i_diagnostic_info.R
+++ tests/testthat/test-ci_bound_i_diagnostic_info.R
@@ -24,11 +24,11 @@ opts0 <- list(ftol_abs = 1e-7,
               )
 system.time(out1l <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound", opts = opts0, verbose = TRUE))
 system.time(out1u <- ci_bound_i(1, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound", opts = opts0, verbose = TRUE))
-system.time(out1ls <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound", opts = opts0, standardized = TRUE, verbose = TRUE))
-system.time(out1us <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound", opts = opts0, standardized = TRUE, verbose = TRUE))
+# system.time(out1ls <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "lbound", opts = opts0, standardized = TRUE, verbose = TRUE))
+# system.time(out1us <- ci_bound_i(2, 5, sem_out = fit_med, f_constr = fn_constr0, which = "ubound", opts = opts0, standardized = TRUE, verbose = TRUE))
 
 out1l_diag <- attr(out1l, "diag")
-out1ls_diag <- attr(out1ls, "diag")
+# out1ls_diag <- attr(out1ls, "diag")
 
 test_that("Check diagnostic object: status", {
     expect_equivalent(
