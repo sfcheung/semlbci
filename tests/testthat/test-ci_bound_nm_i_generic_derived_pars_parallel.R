4534fb629219d3a8049ac10a262db8496a085856
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-04-28 00:09:08 +0800

    WIP: test files for parallel processing
    WIP. Not yet work.

---
+++ tests/testthat/test-ci_bound_nm_i_generic_derived_pars_parallel.R
@@ -0,0 +1,76 @@
+skip("WIP")
+
+library(testthat)
+library(semlbci)
+
+# context("Check ci_bound_nm_i: Derived parameters, test_generic = TRUE")
+
+data(simple_med)
+dat <- simple_med
+mod <- 
+"
+m ~ a*x
+y ~ b*m
+ab:= a*b
+asq:= a^2
+abc:= ab*asq
+am:= mean(c(ab, asq, abc))
+"
+fit_med <- lavaan::sem(mod, simple_med, fixed.x = FALSE)
+lavaan::parameterTable(fit_med)
+
+# opts0 <- list(print_level = 3)
+opts0 <- list()
+opts0 <- list(ftol_abs = 1e-7,
+              ftol_rel = 1e-7,
+              xtol_abs = 1e-7,
+              xtol_rel = 1e-7,
+              tol_constraints_eq = 1e-7
+              )
+system.time(out <- semlbci(fit_med, c(6, 7), opts = opts0, parallel = TRUE, ncpu = 2))
+# system.time({
+#   out1l <- ci_bound_nm_i(6, 5, sem_out = fit_med, which = "lbound", opts = opts0, test_generic = TRUE)
+#   out1u <- ci_bound_nm_i(6, 5, sem_out = fit_med, which = "ubound", opts = opts0, test_generic = TRUE)
+#   out2l <- ci_bound_nm_i(7, 5, sem_out = fit_med, which = "lbound", opts = opts0, test_generic = TRUE)
+#   out2u <- ci_bound_nm_i(7, 5, sem_out = fit_med, which = "ubound", opts = opts0, test_generic = TRUE)
+#   })
+
+library(OpenMx)
+cov_dat <- cov(dat)
+n <- nrow(dat)
+manifest_vars <- c("x", "m", "y")
+mod_mx <- mxModel("Mediation", type = "RAM", 
+    manifestVars = manifest_vars,
+    mxPath(from = "x", to = "m", arrows = 1, free = TRUE, values = 1,
+                  labels = "a"),
+    mxPath(from = "m", to = "y", arrows = 1, free = TRUE, values = 1,
+                  labels = "b"),
+    mxPath(from = "x", arrows = 2, free = TRUE, values = 1,
+                  labels = "var_x"),
+    mxPath(from = "m", arrows = 2, free = TRUE, values = 1,
+                  labels = "evar_m"),
+    mxPath(from = "y", arrows = 2, free = TRUE, values = 1,
+                  labels = "evar_y"),
+    mxAlgebra(a * b , name = "ab"),
+    mxAlgebra(a ^ 2 , name = "asq"),
+    mxCI(reference = c("a", "b", "ab", "asq"), 
+                       interval = .95, type = "both"),
+    mxData(observed = cov_dat, type = "cov", numObs = n)
+  )
+
+# To replicate OpenMx 2.18.1 with SLSQP
+mod_mx <- mxOption(mod_mx, "Feasibility tolerance", "1e-6")
+
+fit_med_OpenMx <- mxRun(mod_mx, silent = TRUE, intervals = TRUE)
+ci_OpenMx <- summary(fit_med_OpenMx)$CI
+
+ci_semlbci <- c(out[c(6, 7), "lbci_lb"], out[c(6, 7), "lbci_ub"])
+
+test_that("Equal to OpenMx LBCI", {
+    expect_equal(
+        ci_semlbci, 
+        unlist(ci_OpenMx[c(3, 4), c("lbound", "ubound")]),
+        tolerance = 1e-3,
+        ignore_attr = TRUE
+      )
+  })
---
+++ tests/testthat/test-ci_bound_nm_i_generic_standardized_parallel.R
@@ -0,0 +1,79 @@
+skip("WIP")
+
+library(testthat)
+library(semlbci)
+
+# context("Check ci_bound_nm_i: Standardized, No equality constraints, test_generic = TRUE")
+
+data(simple_med)
+dat <- simple_med
+mod <- 
+"
+m ~ x
+y ~ m
+"
+fit_med <- lavaan::sem(mod, simple_med, fixed.x = FALSE)
+
+# fn_constr0 <- set_constraint(fit_med)
+
+# opts0 <- list(print_level = 3)
+opts0 <- list()
+opts0 <- list(ftol_abs = 1e-7,
+              ftol_rel = 1e-7,
+              xtol_abs = 1e-7,
+              xtol_rel = 1e-7,
+              tol_constraints_eq = 1e-7
+              )
+system.time(out <- semlbci(fit_med, c(1, 2), standardized = TRUE, opts = opts0, parallel = TRUE, ncpu = 2))
+
+# system.time({
+#   out1l <- ci_bound_nm_i(1, 5, sem_out = fit_med, which = "lbound", standardized = TRUE, opts = opts0, test_generic = TRUE)
+#   out1u <- ci_bound_nm_i(1, 5, sem_out = fit_med, which = "ubound", standardized = TRUE, opts = opts0, test_generic = TRUE)
+#   out2l <- ci_bound_nm_i(2, 5, sem_out = fit_med, which = "lbound", standardized = TRUE, opts = opts0, test_generic = TRUE)
+#   out2u <- ci_bound_nm_i(2, 5, sem_out = fit_med, which = "ubound", standardized = TRUE, opts = opts0, test_generic = TRUE)
+#   })
+
+
+library(OpenMx)
+cov_dat <- cov(dat)
+n <- nrow(dat)
+manifest_vars <- c("x", "m", "y")
+mod_mx <- mxModel("Mediation", type = "RAM", 
+    manifestVars = manifest_vars,
+    mxPath(from = "x", to = "m", arrows = 1, free = TRUE, values = 1,
+                  labels = "a"),
+    mxPath(from = "m", to = "y", arrows = 1, free = TRUE, values = 1,
+                  labels = "b"),
+    mxPath(from = "x", arrows = 2, free = TRUE, values = 1,
+                  labels = "var_x"),
+    mxPath(from = "m", arrows = 2, free = TRUE, values = 1,
+                  labels = "evar_m"),
+    mxPath(from = "y", arrows = 2, free = TRUE, values = 1,
+                  labels = "evar_y"),
+    mxAlgebra(a * b , name = "ab"),
+    mxAlgebra((a^2) * var_x + evar_m, name = "var_m"),
+    mxAlgebra((b^2) * var_m + evar_y, name = "var_y"),
+    mxAlgebra(a * sqrt(var_x) / sqrt(var_m), name = "a_std"),
+    mxAlgebra(b * sqrt(var_m) / sqrt(var_y) , name = "b_std"),
+    mxCI(reference = c("a", "b", "ab", "a_std", "b_std"), 
+                       interval = .95, type = "both"),
+    mxData(observed = cov_dat, type = "cov", numObs = n)
+  )
+
+# To replicate OpenMx 2.18.1 with SLSQP
+mod_mx <- mxOption(mod_mx, "Feasibility tolerance", "1e-6")
+
+fit_med_OpenMx <- mxRun(mod_mx, silent = TRUE, intervals = TRUE)
+ci_OpenMx <- summary(fit_med_OpenMx)$CI
+
+ci_semlbci_old <- c(out1l, out2l, out1u, out2u)
+ci_semlbci <- c(out[c(1, 2), "lbci_lb"], out[c(1, 2), "lbci_ub"])
+
+test_that("Equal to OpenMx LBCI", {
+    expect_equal(
+        ci_semlbci, 
+        unlist(ci_OpenMx[c(4, 5), c("lbound", "ubound")]),
+        tolerance = 1e-3,
+        ignore_attr = TRUE
+      )
+  })
