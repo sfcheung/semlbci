899017d4968eb48f3845f8572b8e38fabd52ccc3
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-05-01 10:45:18 +0800

    Update get_lhs_op_rhs
    It can also return block and group, which are necessary to uniquely identify a parameter in multiple-group SEM.
    Test passed.

--- R/get_lhs_op_rhs.R
+++ R/get_lhs_op_rhs.R
@@ -1,16 +1,21 @@
-#' @title Get lhs, op, and rhs of a parameter from an SEM output
+#' @title Get lhs, op, rhs, and other info of a parameter from an SEM output
 #'
-#' @description Get lhs, op, and rhs of a parameter from an SEM output
+#' @description Get lhs, op, rhs and other information necessary for 
+#'              uniquely identifying a parameter from an SEM output
 #'
 #' @details 
 #' 
 #' Currently supports \code{lavaan} output only.
 #'
 #' @return
-#' A one row data frame of lhs, op, and rhs of the parameter in the parameter table.
+#' A one row data frame of lhs, op, rhs, block, and group of the parameter in 
+#' the parameter table.
+#' 
+#' By default, only return lhs, op, and rhs.
 #' 
 #' @param i The position of the target parameters as in the parameter table of lavaan.
 #' @param sem_out The SEM output. Currently \code{lavaan} output only.
+#' @param more If `TRUE`, block and group are also returned. Default is `FALSE`.
 #'
 #' @examples
 #' library(lavaan)
@@ -25,11 +30,18 @@
 #' fit <- sem(mod, cfa_two_factors)
 #' @keywords internal
 
-get_lhs_op_rhs <- function(i, sem_out) {
+get_lhs_op_rhs <- function(i, sem_out, more = FALSE) {
     if (!inherits(sem_out, "lavaan")) {
         stop("sem_out is not a supported object.")
       }
     ptable <- lavaan::parameterTable(sem_out)
-    # Do not check for 
-    as.data.frame(as.list(ptable[i, c("lhs", "op", "rhs")]))
-  }  
+    # more is added to maintain compatibility with existing fucntions.
+    # They may assume that the output has only three columns.
+    if (more) {
+        return(as.data.frame(
+                as.list(ptable[i, c("lhs", "op", "rhs", "block", "group")])
+              ))
+      } else {
+        return(as.data.frame(as.list(ptable[i, c("lhs", "op", "rhs")])))
+      }
+  }
---
+++ tests/testthat/test-get_lhs_op_rhs.R
@@ -0,0 +1,51 @@
+skip("WIP")
+
+library(testthat)
+library(semlbci)
+
+library(lavaan)
+
+dat <- HolzingerSwineford1939
+mod <- "
+  visual  =~ x1 + x2 + x3
+  textual =~ x4 + x5 + x6
+  speed   =~ x7 + x8 + x9
+"
+
+fit_lavaan <- cfa(mod, dat)
+
+out1 <- get_lhs_op_rhs(i = 6, fit_lavaan)
+out2 <- get_lhs_op_rhs(i = 6, fit_lavaan, more = TRUE)
+
+test_that("Check output in one-group CFA", {
+    expect_equal(
+        out1,
+        data.frame(lhs = "textual", op = "=~", rhs = "x6")
+      )
+    expect_equal(
+        out2,
+        data.frame(lhs = "textual", op = "=~", rhs = "x6",
+                   block = 1, group = 1)
+      )
+  })
+
+fit_lavaan_gp <- cfa(mod, dat, group = "school")
+
+
+summary(fit)
+
+
+out1gp <- get_lhs_op_rhs(i = 58, fit_lavaan_gp)
+out2gp <- get_lhs_op_rhs(i = 58, fit_lavaan_gp, more = TRUE)
+
+test_that("Check output in two-group CFA", {
+    expect_equal(
+        out1gp,
+        data.frame(lhs = "visual", op = "~~", rhs = "textual")
+      )
+    expect_equal(
+        out2gp,
+        data.frame(lhs = "visual", op = "~~", rhs = "textual",
+                   block = 2, group = 2)
+      )
+  })
