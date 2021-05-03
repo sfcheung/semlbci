c8e778e568e45f91d295cb1cf0aca4ae177d75b7
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-05-03 13:10:04 +0800

    Update test-ci_bound_wn_i_cfa_eq_constraints.R
    Test passed

--- tests/testthat/test-ci_bound_wn_i_cfa_eq_constraints.R
+++ tests/testthat/test-ci_bound_wn_i_cfa_eq_constraints.R
@@ -3,16 +3,17 @@ skip("WIP: Tests not passed or tests not ready. To fix")
 library(testthat)
 library(semlbci)
 
-# Do CFA later. Too slow for lavaan
-
+library(lavaan)
 data(cfa_two_factors)
 dat <- cfa_two_factors
 mod <- 
 "
-f1 =~ x1 + x2 + x3
-f2 =~ x4 + x5 + x6
+f1 =~ x1 + b*x2 + c*x3
+f2 =~ x4 + d*x5 + e*x6
+b == d
 "
 fit_cfa <- lavaan::sem(mod, dat)
+parameterEstimates(fit_cfa)
 parameterTable(fit_cfa)
 
 fn_constr0 <- set_constraint(fit_cfa)
@@ -24,23 +25,25 @@ opts0 <- list(ftol_abs = 1e-7,
               xtol_abs = 1e-7,
               xtol_rel = 1e-7
               )
-system.time(out2l <- ci_bound_wn_i(2, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
-system.time(out2u <- ci_bound_wn_i(2, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
-system.time(out3l <- ci_bound_wn_i(3, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
-system.time(out3u <- ci_bound_wn_i(3, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
-system.time(out5l <- ci_bound_wn_i(5, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
-system.time(out5u <- ci_bound_wn_i(5, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
-system.time(out6l <- ci_bound_wn_i(6, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
-system.time(out6u <- ci_bound_wn_i(6, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
+system.time(out03l <- ci_bound_wn_i(3, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
+system.time(out03u <- ci_bound_wn_i(3, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
+system.time(out05l <- ci_bound_wn_i(5, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
+system.time(out05u <- ci_bound_wn_i(5, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
+system.time(out06l <- ci_bound_wn_i(6, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
+system.time(out06u <- ci_bound_wn_i(6, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
+system.time(out15l <- ci_bound_wn_i(15, 13, sem_out = fit_cfa, which = "lbound", opts = opts0, f_constr = fn_constr0))
+system.time(out15u <- ci_bound_wn_i(15, 13, sem_out = fit_cfa, which = "ubound", opts = opts0, f_constr = fn_constr0))
 
 
 modc0 <- 
 "
 f1 =~ x1 + b*x2 + c*x3
 f2 =~ x4 + d*x5 + e*x6
+b == d
 "
 
-modc <- paste(modc0, "\nb == ", out02l)
+modc <- paste(modc0, "\nc == ", out03l)
+cat(modc)
 fit_cfac <- lavaan::cfa(modc, dat)
 anova(fit_cfac, fit_cfa)
 
@@ -48,12 +51,70 @@ test_that("Check p-value for the chi-square difference test", {
     expect_equal(
         anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
         .05,
-        tolerance = 1e-6,
+        tolerance = 1e-5,
         ignore_attr = TRUE
       )
   })
+
+modc <- paste(modc0, "\nc == ", out03u)
+cat(modc)
+fit_cfac <- lavaan::cfa(modc, dat)
+anova(fit_cfac, fit_cfa)
+
+test_that("Check p-value for the chi-square difference test", {
+    expect_equal(
+        anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
+        .05,
+        tolerance = 1e-5,
+        ignore_attr = TRUE
+      )
+  })
+
+modc <- paste(modc0, "\nd == ", out05l)
+cat(modc)
+fit_cfac <- lavaan::cfa(modc, dat)
+anova(fit_cfac, fit_cfa)
+
+test_that("Check p-value for the chi-square difference test", {
+    expect_equal(
+        anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
+        .05,
+        tolerance = 1e-5,
+        ignore_attr = TRUE
+      )
+  })
+
+modc <- paste(modc0, "\nd == ", out05u)
+cat(modc)
+fit_cfac <- lavaan::cfa(modc, dat)
+anova(fit_cfac, fit_cfa)
+
+test_that("Check p-value for the chi-square difference test", {
+    expect_equal(
+        anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
+        .05,
+        tolerance = 1e-5,
+        ignore_attr = TRUE
+      )
+  })
+
+modc <- paste(modc0, "\ne == ", out06l)
+cat(modc)
+fit_cfac <- lavaan::cfa(modc, dat)
+anova(fit_cfac, fit_cfa)
+
+test_that("Check p-value for the chi-square difference test", {
+    expect_equal(
+        anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
+        .05,
+        tolerance = 1e-3,
+        ignore_attr = TRUE
+      )
+  })
+
 
 modc <- paste(modc0, "\ne == ", out06u)
+cat(modc)
 fit_cfac <- lavaan::cfa(modc, dat)
 anova(fit_cfac, fit_cfa)
 
@@ -61,10 +122,48 @@ test_that("Check p-value for the chi-square difference test", {
     expect_equal(
         anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
         .05,
-        tolerance = 1e-6,
+        tolerance = 1e-3,
         ignore_attr = TRUE
       )
   })
 
 
+
+modc0 <- 
+"
+f1 =~ x1 + b*x2 + c*x3
+f2 =~ x4 + d*x5 + e*x6
+f1 ~~ fr*f2
+b == d
+"
+
+
+modc <- paste(modc0, "\nfr == ", out15l)
+cat(modc)
+fit_cfac <- lavaan::cfa(modc, dat)
+anova(fit_cfac, fit_cfa)
+
+test_that("Check p-value for the chi-square difference test", {
+    expect_equal(
+        anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
+        .05,
+        tolerance = 1e-3,
+        ignore_attr = TRUE
+      )
+  })
+
+
+modc <- paste(modc0, "\nfr == ", out15u)
+cat(modc)
+fit_cfac <- lavaan::cfa(modc, dat)
+anova(fit_cfac, fit_cfa)
+
+test_that("Check p-value for the chi-square difference test", {
+    expect_equal(
+        anova(fit_cfac, fit_cfa)[2, "Pr(>Chisq)"], 
+        .05,
+        tolerance = 1e-3,
+        ignore_attr = TRUE
+      )
+  })
 
