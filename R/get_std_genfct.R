d614ecffa4a550a5dd2594f1c228ae000f3edb93
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-05-08 12:42:29 +0800

    Create get_std_genfct.R
    An internal helper function for test. It finds the standardized estimate of a parameter to be constrained.

---
+++ R/get_std_genfct.R
@@ -0,0 +1,43 @@
+#' @title Generate a function to get the standardized estiamte of a parameter 
+#'
+#' @description Generate a function to get the standardized estiamte of a parameter 
+#'
+#' @return
+#' A function to be used in a lavaan model.
+#' 
+#' @param fit The source fit object.
+#' @param i The position of the standardized parameter in the parameter table.
+#'
+#' @examples
+#' # To Do
+#'
+#' @keywords internal
+
+get_std_genfct <- function(fit, i) {
+    fit_pt <- lavaan::parameterTable(fit)
+    tmpfct <- function(...) {
+        force(fit)
+        force(i)
+        force(fit_pt)
+        .x. <- get(".x.", envir = parent.frame())
+        fit@Model <- lavaan::lav_model_set_parameters(
+                          fit@Model, .x.
+                        )
+        fit_pt2 <- fit_pt
+        fit_pt2[fit_pt$free > 0, "est"] <- .x.
+        fit@ParTable <- as.list(fit_pt2)
+        std <- lavaan::standardizedSolution(
+                          fit,
+                          se = FALSE,
+                          zstat = FALSE,
+                          pvalue = FALSE,
+                          ci = FALSE,
+                          cov.std = FALSE,
+                          remove.eq = FALSE,
+                          remove.ineq = FALSE,
+                          remove.def = FALSE,
+                          )
+        std[i, "est.std"]
+      }
+    return(tmpfct)
+  }
