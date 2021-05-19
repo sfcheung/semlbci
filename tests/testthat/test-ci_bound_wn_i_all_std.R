9523d0a550b5c5f591d4babafe3d03bc197059f6
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-05-19 09:54:01 +0800

    Try to make these tests run out of an interactive session
    WIP. Not yet work. Run them interactively.

--- tests/testthat/test-ci_bound_wn_i_all_std.R
+++ tests/testthat/test-ci_bound_wn_i_all_std.R
@@ -1,13 +1,22 @@
-skip("To be tested in interactive sessions only due to scoping issues")
-
-# Not yet work.
+skip_if(Sys.getenv("SEMLBCI_TEST_SLOW") == "",
+        "Skip due to speed or other issues")
+# To be tested in interactive sessions only due to scoping or speed issues
+# Not yet work. Passed in interactive sessions but failed in test_file().
 
 library(testthat)
 
 test_list <-
-  c(dir("./tests/testthat/", pattern = "*_std*.R", full.names = TRUE),
-    dir("./tests/testthat/", pattern = "*_std_*.R", full.names = TRUE))
+  c(dir("./tests/testthat/",
+        pattern = glob2rx("*_i_std*.R"),
+        full.names = TRUE),
+    dir("./tests/testthat/",
+        pattern = glob2rx("*_i_std_*.R"),
+        full.names = TRUE))
 
-Sys.setenv(run_interactive = TRUE)
-out <- lapply(test_list, test_file, env = .GlobalEnv)
-Sys.unsetenv("run_interactive")
+Sys.setenv("SEMLBCI_TEST_SLOW" = "true")
+
+for (i in test_list) {
+    test_file(i)
+  }
+
+Sys.setenv("SEMLBCI_TEST_SLOW" = "")
--- tests/testthat/test-ci_bound_wn_i_std_cfa_user_eq.R
+++ tests/testthat/test-ci_bound_wn_i_std_cfa_user_eq.R
@@ -1,4 +1,6 @@
-skip("To be tested in interactive sessions only due to scoping issues")
+skip_if(Sys.getenv("SEMLBCI_TEST_SLOW") == "",
+        "Skip due to speed or other issues")
+# To be tested in interactive sessions only due to scoping or speed issues
 
 library(testthat)
 library(semlbci)
