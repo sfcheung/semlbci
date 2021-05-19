7d5a62939966504c27f3c27d6faa199ea3991476
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-05-19 18:34:02 +0800

    Update get_std_i.R
    It works if used outside scaling_factor. However, it does not work well with lavaan::update called inside another function.

--- R/get_std_i.R
+++ R/get_std_i.R
@@ -11,35 +11,41 @@
 #' @examples
 #' # TODO
 #'
-#' @export
+#' @keywords internal
 
 get_std_i <- function(i, ...) {
     .x. <- get(".x.", envir = parent.frame())
-    i_main <- 1
-    while (!exists("lavoptions", parent.frame(i_main)) &
-           !exists("PARTABLE", parent.frame(i_main))) {
-        i_main <- i_main + 1
-        if (i_main > 10) browser()
-      } 
-    if (!exists("get_std_i_env", parent.frame(i_main))) {
-        assign("get_std_i_env", new.env(), pos = parent.frame(i_main))
-        i_tmp <- 1
-        while (!exists("lavmodel", parent.frame(i_tmp))) {
-            i_tmp <- i_tmp + 1
-            if (i_tmp > 10) browser()
-          }
-        assign("lavmodel_tmp", get("lavmodel", parent.frame(i_tmp)),
-               pos = parent.frame(i_main)$get_std_i_env)
-        i_tmp <- 1
-        while (!exists("lavpartable", parent.frame(i_tmp))) {
-            i_tmp <- i_tmp + 1
-            if (i_tmp > 10) browser()
-          }
-        assign("pt_tmp", get("lavpartable", parent.frame(i_tmp)),
-               pos = parent.frame(i_main)$get_std_i_env)
+    # i_main <- 1
+    # while (!exists("lavoptions", parent.frame(i_main)) &
+    #        !exists("PARTABLE", parent.frame(i_main))) {
+    #     i_main <- i_main + 1
+    #     if (i_main > 10) browser()
+    #   } 
+    # if (!exists("get_std_i_env", parent.frame(i_main))) {
+    #     assign("get_std_i_env", new.env(), pos = parent.frame(i_main))
+    #     i_tmp <- 1
+    #     while (!exists("lavmodel", parent.frame(i_tmp))) {
+    #         i_tmp <- i_tmp + 1
+    #         if (i_tmp > 10) browser()
+    #       }
+    #     assign("lavmodel_tmp", get("lavmodel", parent.frame(i_tmp)),
+    #            pos = parent.frame(i_main)$get_std_i_env)
+    #     i_tmp <- 1
+    #     while (!exists("lavpartable", parent.frame(i_tmp))) {
+    #         i_tmp <- i_tmp + 1
+    #         if (i_tmp > 10) browser()
+    #       }
+    #     assign("pt_tmp", get("lavpartable", parent.frame(i_tmp)),
+    #            pos = parent.frame(i_main)$get_std_i_env)
+    #   }
+    # lavmodel_tmp <- get("lavmodel_tmp", parent.frame(i_main)$get_std_i_env)
+    # pt_tmp <- get("pt_tmp", parent.frame(i_main)$get_std_i_env)
+    i_tmp2 <- 1
+    while (!exists("lavpartable", parent.frame(i_tmp2))) {
+        i_tmp2 <- i_tmp2 + 1
+        if (i_tmp2 > 50) browser()
       }
-    lavmodel_tmp <- get("lavmodel_tmp", parent.frame(i_main)$get_std_i_env)
-    pt_tmp <- get("pt_tmp", parent.frame(i_main)$get_std_i_env)
+    pt_tmp <- get("lavpartable", parent.frame(i_tmp2))
     lor <- c(pt_tmp$lhs[i], pt_tmp$op[i], pt_tmp$rhs[i],
              pt_tmp$block[i], pt_tmp$group[i])
     pt_tmp <- as.data.frame(pt_tmp)
