cc0ba86a344fe3c77221e93a3e060da90fb1ce80
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-05-23 21:22:36 +0800

    Update test-semlbci_wn_parallel_2.R
    Remove the lines on OpenMx, which are not used in this test

--- tests/testthat/test-semlbci_wn_parallel_2.R
+++ tests/testthat/test-semlbci_wn_parallel_2.R
@@ -16,13 +16,4 @@ fit <- lavaan::sem(mod, simple_med, fixed.x = FALSE)
 
 lbci_med <- semlbci(fit, parallel = TRUE, ncpus = 5)
 
-test_that("Equal to OpenMx LBCIs for free parameters", {
-    expect_equal(
-        as.numeric(unlist(lbci_med[c(1, 2), c("lbci_lb", "lbci_ub")])), 
-        unlist(ci_OpenMx[c("a", "b"), c("lbound", "ubound")]) ,
-        tolerance = 1e-5,
-        ignore_attr = TRUE
-      )
-  })
-
 
