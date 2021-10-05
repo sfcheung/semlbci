edb54b03cb18f56d9a7ace39edaa7b509a462dd1
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-10-05 21:21:01 +0800

    Fix the doc of sample datasets

--- R/data.R
+++ R/data.R
@@ -1,48 +1,50 @@
-#' Dataset: Two factors, six variables, 500 cases
+#' @title Dataset (CFA, Two Factors, Six Variables)
 #'
-#' Generated from a two-factor model with six variables, n = 500
+#' @description Generated from a two-factor model with six variables, n = 500
 #'
-#' @format A data framw with 500 rows and six variables.
+#' @details
+#' Supposed to be tested using this example:
 #'
-#' @examples
-#' \dontrun{
+#' ```
+#' library(lavaan)
+#' mod <- "f1 =~ x1 + x2 + x3
+#'         f2 =~ x4 + x5 + x6"
+#' fit <- cfa(mod, cfa_two_factors)
+#' summary(fit)
+#' ```
 #'
-#'  library(lavaan)
-#'  mod <- "f1 =~ x1 + x2 + x3
-#'          f2 =~ x4 + x5 + x6"
-#'  fit <- cfa(mod, cfa_two_factors)
-#'  summary(fit)
-#' }
-
+#' @format A data frame with 500 rows and six variables, `x1` to `x6`.
+#'
 "cfa_two_factors"
 
-#' Dataset: Simple mediation model, one IV, one DV, one mediation, 500 cases
+#' @title Dataset (Simple Mediation Model)
 #'
-#' Generated from a simple mediation model, n = 500
+#' @description Generated from a simple mediation model, n = 500
 #'
-#' @format A data framw with 500 rows and three variables:
+#' @format A data frame with 500 rows and three variables:
 #' \describe{
 #'    \item{x}{x, the independent variable}
-#'    \item{m}{m, the mediatior}
+#'    \item{m}{m, the mediator}
 #'    \item{y}{y, the dependent variable}
 #' }
 #'
-#' @examples
-#' \dontrun{
+#' @details
+#' Supposed to be tested using this example:
 #'
-#'  library(lavaan)
-#'  mod <- "m ~ x
-#'          y ~ m"
-#'  fit <- cfa(mod, simple_med)
-#'  summary(fit)
-#' }
+#' ```
+#' library(lavaan)
+#' mod <- "m ~ x
+#'         y ~ m"
+#' fit <- cfa(mod, simple_med)
+#' summary(fit)
+#' ```
 #'
 "simple_med"
 
-#' Dataset: Five-predictor regression model with one correlation close to one
+#' @title Dataset (Six Variables, One Correlation Close to One)
 #'
-#' Generated from a regression model six variables, with correlation
-#' between x4 and x5 close to one.
+#' @description Generated from a regression model six variables,
+#'  x4~~x5 correlation close to one.
 #'
 #' @format A data frame with 100 rows and six variables:
 #' \describe{
@@ -54,42 +56,38 @@
 #'    \item{y}{y, the dependent variable}
 #' }
 #'
-#' @examples
-#' \dontrun{
+#' @details
+#' Supposed to be tested using this example:
 #'
-#'  out <- lm(y ~ x1 + x2 + x3 + x4 + x5, reg_cor_near_one)
-#'  summary(out)
-#'  cor(reg_cor_near_one[, c("x4", "x5")])
-#'
-#' }
+#' ```
+#' out <- lm(y ~ x1 + x2 + x3 + x4 + x5, reg_cor_near_one)
+#' summary(out)
+#' cor(reg_cor_near_one[, c("x4", "x5")])
+#' ```
 #'
 "reg_cor_near_one"
 
-#' Dataset: Two factors with one standardied error variance close to zero.
+#' @title Dataset (CFA, Two Factors, One Standardied Error Variance
+#'  Close to Zero)
 #'
-#' Generated from a two-factor model, with one standardized error variacne
-#' close to zero.
+#' @description Generated from a two-factor model, with one
+#'  standardized error variance close to zero.
 #'
-#' @format A data frame with 100 rows and six variables:
-#' \describe{
-#'    \item{x1}{x1}
-#'    \item{x2}{x2}
-#'    \item{x3}{x3}
-#'    \item{x4}{x4}
-#'    \item{x5}{x5}
-#'    \item{x6}{x6}
-#' }
+#' @format A data frame with 100 rows and six variables, `x1` to `x6`
 #'
-#' @examples
-#' \dontrun{
-#'  # If fitted by the following model, the standardized
-#'  # error variance of `x3` is close to zero.
-#'  # Consequnently, the R-square of `x3` is close to one:
+#' @details
+#' Supposed to be tested using this example:
 #'
-#'  library(lavaan)
-#'  mod <- "f1 =~ x1 + x2 + x3
-#'          f2 =~ x4 + x5 + x6"
-#'  fit <- cfa(mod, cfa_evar_near_zero)
-#'  summary(fit, standardized = TRUE, rsquare = TRUE)
-#' }
+#' ```
+#' # If fitted by the following model, the standardized
+#' # error variance of `x3` is close to zero.
+#' # Consequently, the R-square of `x3` is close to one:
+#'
+#' library(lavaan)
+#' mod <- "f1 =~ x1 + x2 + x3
+#'         f2 =~ x4 + x5 + x6"
+#' fit <- cfa(mod, cfa_evar_near_zero)
+#' summary(fit, standardized = TRUE, rsquare = TRUE)
+#' ```
+#'
 "cfa_evar_near_zero"
