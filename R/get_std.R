b2317270579d311cde718b9672180ee6828b9e9b
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-10-14 15:50:40 +0800

    Test get_std with internal standardization
    Test passed

--- R/get_std.R
+++ R/get_std.R
@@ -11,6 +11,13 @@
 #'
 #' @param i The position of the parameter in the parameter table.
 #'
+#' @param std_method The method used to find the standardized solution.
+#'  If equal to `"lavaan"``, [lavaan::standardizedSolution()] will be used.
+#'  If equal to `"internal"`, an internal function of this package will be used.
+#'  The `"lavaan"` method should work in all situations, but the `"internal"`
+#'  method can be faster. Default is `"lavaan"` for now, but may be changed to
+#'  `"internal"` if it is confirmed to work in all situations tested.
+#'
 #' @examples
 #' data(simple_med)
 #'
@@ -27,27 +34,37 @@
 #'
 #' @export
 
-get_std <- function(fit_str, i) {
+get_std <- function(fit_str, i, std_method = "lavaan") {
     fit <- eval(get(fit_str), parent.frame())
     fit_pt <- lavaan::parameterTable(fit)
     .x. <- get(".x.", envir = parent.frame())
-    fit@Model <- lavaan::lav_model_set_parameters(
-                      fit@Model, .x.
-                    )
-    fit_pt2 <- fit_pt
-    nfree <- sum(fit_pt$free > 0)
-    fit_pt2[fit_pt$free > 0, "est"] <- .x.[seq_len(nfree)]
-    fit@ParTable <- as.list(fit_pt2)
-    std <- lavaan::standardizedSolution(
-                      fit,
-                      se = FALSE,
-                      zstat = FALSE,
-                      pvalue = FALSE,
-                      ci = FALSE,
-                      cov.std = FALSE,
-                      remove.eq = FALSE,
-                      remove.ineq = FALSE,
-                      remove.def = FALSE,
-                      )
-    std[i, "est.std"]
+
+    # lavaan::standardizedSolution
+    if (std_method == "lavaan") {
+        fit@Model <- lavaan::lav_model_set_parameters(
+                          fit@Model, .x.
+                        )
+        fit_pt2 <- fit_pt
+        nfree <- sum(fit_pt$free > 0)
+        fit_pt2[fit_pt$free > 0, "est"] <- .x.[seq_len(nfree)]
+        fit@ParTable <- as.list(fit_pt2)
+        std <- lavaan::standardizedSolution(
+                          fit,
+                          se = FALSE,
+                          zstat = FALSE,
+                          pvalue = FALSE,
+                          ci = FALSE,
+                          cov.std = FALSE,
+                          remove.eq = FALSE,
+                          remove.ineq = FALSE,
+                          remove.def = FALSE,
+                          )
+        return(std[i, "est.std"])
+      }
+
+    # Internal standardization
+    if (std_method == "internal") {
+        return(std_lav(.x., fit)[i])
+      }
+    stop("std_method unknown.")
   }
---
+++ tests/testthat/test-get_std.R
@@ -0,0 +1,68 @@
+skip("WIP: Not ready")
+
+library(testthat)
+library(semlbci)
+options(width = 132)
+# Fit model
+
+library(lavaan)
+
+data(cfa_two_factors)
+dat <- cfa_two_factors
+mod <-
+"
+f1 =~ x1 + x2 + x3
+f2 =~ x4 + x5 + x6
+f1 ~ f2
+"
+fit <- lavaan::sem(mod, cfa_two_factors)
+
+# std_method: lavaan
+
+fit00 <- lavaan::update(fit,
+                        model = parameterTable(fit),
+                        add = paste0("tmp", " := ",
+                                    "semlbci::get_std('",
+                                    "fit", "',", 7,
+                                    ", std_method = ",
+                                    sQuote("lavaan"),
+                                    ")"),
+                        do.fit = TRUE,
+                        baseline = FALSE,
+                        h1 = FALSE,
+                        se = "none")
+
+parameterEstimates(fit)
+parameterEstimates(fit00)
+
+test_that("Check user computed standardized solution", {
+    expect_equal(parameterEstimates(fit00)[16, "est"],
+                 standardizedSolution(fit, se = FALSE)[7, "est.std"],
+                 tolerance = 1e-5)
+  })
+
+
+# std_method: internal
+
+fit00 <- lavaan::update(fit,
+                        model = parameterTable(fit),
+                        add = paste0("tmp", " := ",
+                                    "semlbci::get_std('",
+                                    "fit", "',", 7,
+                                    ", std_method = ",
+                                    sQuote("internal"),
+                                    ")"),
+                        do.fit = TRUE,
+                        baseline = FALSE,
+                        h1 = FALSE,
+                        se = "none")
+
+parameterEstimates(fit)
+parameterEstimates(fit00)
+
+test_that("Check user computed standardized solution", {
+    expect_equal(parameterEstimates(fit00)[16, "est"],
+                 standardizedSolution(fit, se = FALSE)[7, "est.std"],
+                 tolerance = 1e-5)
+  })
+
