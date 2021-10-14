c0f5493ea0f437e85cef44791009ece3d93ad304
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-10-14 16:24:27 +0800

    get_std will fall back to lavaan method if the internal method failed

--- R/get_std.R
+++ R/get_std.R
@@ -16,7 +16,9 @@
 #'  If equal to `"internal"`, an internal function of this package will be used.
 #'  The `"lavaan"` method should work in all situations, but the `"internal"`
 #'  method can be faster. Default is `"lavaan"` for now, but may be changed to
-#'  `"internal"` if it is confirmed to work in all situations tested.
+#'  `"internal"` if it is confirmed to work in all situations tested. Note that
+#'  if the internal method failed, the [get_std()] will fall back to the
+#'  lavaan method.
 #'
 #' @examples
 #' data(simple_med)
@@ -39,32 +41,34 @@ get_std <- function(fit_str, i, std_method = "lavaan") {
     fit_pt <- lavaan::parameterTable(fit)
     .x. <- get(".x.", envir = parent.frame())
 
-    # lavaan::standardizedSolution
-    if (std_method == "lavaan") {
-        fit@Model <- lavaan::lav_model_set_parameters(
-                          fit@Model, .x.
-                        )
-        fit_pt2 <- fit_pt
-        nfree <- sum(fit_pt$free > 0)
-        fit_pt2[fit_pt$free > 0, "est"] <- .x.[seq_len(nfree)]
-        fit@ParTable <- as.list(fit_pt2)
-        std <- lavaan::standardizedSolution(
-                          fit,
-                          se = FALSE,
-                          zstat = FALSE,
-                          pvalue = FALSE,
-                          ci = FALSE,
-                          cov.std = FALSE,
-                          remove.eq = FALSE,
-                          remove.ineq = FALSE,
-                          remove.def = FALSE,
-                          )
-        return(std[i, "est.std"])
-      }
-
     # Internal standardization
     if (std_method == "internal") {
-        return(std_lav(.x., fit)[i])
+        out <- try(std_lav(.x., fit)[i], silent = TRUE)
+        if (!inherits(out, "try-error")) {
+            return(out)
+          }
       }
-    stop("std_method unknown.")
+
+    # lavaan::standardizedSolution
+    # This method will be used if the internal method failed.
+
+    fit@Model <- lavaan::lav_model_set_parameters(
+                      fit@Model, .x.
+                    )
+    fit_pt2 <- fit_pt
+    nfree <- sum(fit_pt$free > 0)
+    fit_pt2[fit_pt$free > 0, "est"] <- .x.[seq_len(nfree)]
+    fit@ParTable <- as.list(fit_pt2)
+    std <- lavaan::standardizedSolution(
+                      fit,
+                      se = FALSE,
+                      zstat = FALSE,
+                      pvalue = FALSE,
+                      ci = FALSE,
+                      cov.std = FALSE,
+                      remove.eq = FALSE,
+                      remove.ineq = FALSE,
+                      remove.def = FALSE,
+                      )
+    return(std[i, "est.std"])
   }
