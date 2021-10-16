1f81531879f9bf25f9fa1bb225982eab5597a71a
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-10-16 16:27:46 +0800

    Delete an obsolete test and update the name of a test

--- tests/testthat/test-semlbci_wn_parallel.R
+++ tests/testthat/test-semlbci_wn_parallel.R
@@ -12,56 +12,8 @@ mod <-
 m ~ x
 y ~ m
 "
-fit_med <- lavaan::sem(mod, simple_med, fixed.x = FALSE)
+fit <- lavaan::sem(mod, simple_med, fixed.x = FALSE)
 
-library(OpenMx)
-cov_dat <- cov(dat)
-n <- nrow(dat)
-manifest_vars <- c("x", "m", "y")
-mod_mx <- mxModel("Mediation", type = "RAM", 
-    manifestVars = manifest_vars,
-    mxPath(from = "x", to = "m", arrows = 1, free = TRUE, values = 1,
-                  labels = "a"),
-    mxPath(from = "m", to = "y", arrows = 1, free = TRUE, values = 1,
-                  labels = "b"),
-    mxPath(from = "x", arrows = 2, free = TRUE, values = 1,
-                  labels = "var_x"),
-    mxPath(from = "m", arrows = 2, free = TRUE, values = 1,
-                  labels = "evar_m"),
-    mxPath(from = "y", arrows = 2, free = TRUE, values = 1,
-                  labels = "evar_y"),
-    mxAlgebra(a * b , name = "ab"),
-    mxCI(reference = c("a", "b", "ab", "evar_m"), 
-                       interval = .95, type = "both"),
-    mxData(observed = cov_dat, type = "cov", numObs = n)
-  )
-
-# To replicate OpenMx 2.18.1 with SLSQP
-mod_mx <- mxOption(mod_mx, "Feasibility tolerance", "1e-6")
-
-fit_med_OpenMx <- mxRun(mod_mx, silent = TRUE, intervals = TRUE)
-ci_OpenMx <- summary(fit_med_OpenMx)$CI
-
-lbci_med <- semlbci(fit_med, method = "wn", parallel = TRUE, ncpus = 5)
-
-test_that("Equal to OpenMx LBCIs for free parameters", {
-    expect_equal(
-        as.numeric(unlist(lbci_med[c(1, 2), c("lbci_lb", "lbci_ub")])), 
-        unlist(ci_OpenMx[c("a", "b"), c("lbound", "ubound")]) ,
-        tolerance = 1e-5,
-        ignore_attr = TRUE
-      )
-  })
-
-lbci_med2 <- semlbci(fit_med, pars = c(1, 3), method = "wn", parallel = TRUE, ncpus = 5)
-
-test_that("Check whether only selectd parameters were processed", {
-    expect_equal(
-        as.numeric(unlist(lbci_med2[c(1), c("lbci_lb", "lbci_ub")])), 
-        unlist(ci_OpenMx[c("a"), c("lbound", "ubound")]) ,
-        tolerance = 1e-5,
-        ignore_attr = TRUE
-      )
-  })
+lbci_med <- semlbci(fit, parallel = TRUE, ncpus = 5)
 
 
