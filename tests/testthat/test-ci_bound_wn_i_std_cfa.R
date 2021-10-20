ac29bfb4ba1ac5a05b067ffbf81d3c9d7dd8c73c
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-10-21 00:32:30 +0800

    Test against known answers

--- tests/testthat/test-ci_bound_wn_i_std_cfa.R
+++ tests/testthat/test-ci_bound_wn_i_std_cfa.R
@@ -1,5 +1,4 @@
 skip_on_cran()
-# To fix: Do not use saved data
 
 library(testthat)
 library(semlbci)
@@ -27,22 +26,33 @@ fn_constr0 <- set_constraint(fit, ciperc = ciperc)
 
 # opts0 <- list(print_level = 3)
 opts0 <- list()
-opts0 <- list(ftol_abs = 1e-7,
-              ftol_rel = 1e-7,
-              xtol_abs = 1e-7,
-              xtol_rel = 1e-7,
-              tol_constraints_eq = 1e-10
+opts0 <- list(#ftol_abs = 1e-7,
+              ftol_rel = 1e-5
+              # xtol_abs = 1e-7,
+              # xtol_rel = 1e-7,
+              # tol_constraints_eq = 1e-10
               )
 time1l <- system.time(out1l <- ci_bound_wn_i(2, 13, sem_out = fit, f_constr = fn_constr0, which = "lbound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
-time1u <- system.time(out1u <- ci_bound_wn_i(2, 13, sem_out = fit, f_constr = fn_constr0, which = "ubound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
-time2l <- system.time(out2l <- ci_bound_wn_i(6, 13, sem_out = fit, f_constr = fn_constr0, which = "lbound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
+# time1u <- system.time(out1u <- ci_bound_wn_i(2, 13, sem_out = fit, f_constr = fn_constr0, which = "ubound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
+# time2l <- system.time(out2l <- ci_bound_wn_i(6, 13, sem_out = fit, f_constr = fn_constr0, which = "lbound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
 time2u <- system.time(out2u <- ci_bound_wn_i(6, 13, sem_out = fit, f_constr = fn_constr0, which = "ubound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
 time3l <- system.time(out3l <- ci_bound_wn_i(15, 13, sem_out = fit, f_constr = fn_constr0, which = "lbound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
-time3u <- system.time(out3u <- ci_bound_wn_i(15, 13, sem_out = fit, f_constr = fn_constr0, which = "ubound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
+# time3u <- system.time(out3u <- ci_bound_wn_i(15, 13, sem_out = fit, f_constr = fn_constr0, which = "ubound", verbose = TRUE, ciperc = ciperc, standardized = TRUE, wald_ci_start = FALSE, std_method = "internal"))
 
 timexx <- rbind(time1l, time1u, time2l, time2u, time3l, time3u)
 timexx
 colSums(timexx)
+
+test_that("Check against precomputed answers", {
+    expect_equal(out1l$bound, 0.6745196, tolerance = 1e-5)
+    expect_equal(out2u$bound, 0.8769257, tolerance = 1e-5)
+    expect_equal(out3l$bound, 0.4039171, tolerance = 1e-5)
+    # expect_equal(out1u$bound, 0.8669971, tolerance = 1e-5)
+    # expect_equal(out2l$bound, 0.7460543, tolerance = 1e-5)
+    # expect_equal(out3u$bound, 0.615133, tolerance = 1e-5)
+  })
+
+skip("Run only if data changed")
 
 # Not yet have a way to find how to make test_constr work in standardized solution
 
@@ -50,8 +60,8 @@ test_p <- function(fit0, fit1, ciperc, tol) {
     abs(anova(fit0, fit1)[2, "Pr(>Chisq)"] - (1 - ciperc)) < tol
   }
 
-gen_test_data <- FALSE
-if (gen_test_data) {
+# gen_test_data <- FALSE
+# if (gen_test_data) {
 
 geteststd <- get_std_genfct(fit = fit, i = 2)
 
@@ -142,16 +152,16 @@ fitc <- update(fitc, start = ptable, do.fit = TRUE, baseline = FALSE, h1 = FALSE
                    control = list(eval.max = 2, control.outer = list(tol = 1e-02)))
 fitc_out3u <- fitc
 
-save(fitc_out1l, fitc_out1u,
-     fitc_out2l, fitc_out2u,
-     fitc_out3l, fitc_out3u,
-     file = "inst/testdata/test-ci_bound_wn_i_std_cfa.RData",
-     compress = "xz",
-     compression_level = 9)
-}
+# save(fitc_out1l, fitc_out1u,
+#      fitc_out2l, fitc_out2u,
+#      fitc_out3l, fitc_out3u,
+#      file = "inst/testdata/test-ci_bound_wn_i_std_cfa.RData",
+#      compress = "xz",
+#      compression_level = 9)
+# }
 
-load(system.file("testdata", "test-ci_bound_wn_i_std_cfa.RData",
-                  package = "semlbci"))
+# load(system.file("testdata", "test-ci_bound_wn_i_std_cfa.RData",
+#                   package = "semlbci"))
 
 test_that("Check p-value for the chi-square difference test", {
     expect_true(test_p(fitc_out1l, fit, ciperc = ciperc, tol = 1e-4))
