d40969c2d941b01c985ea9e6483478b4ef4abeb7
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-05-27 15:25:07 +0800

    Update find_variance_in_free.R
    Add find_variance_in_free_lb to set the lower bound for variances

--- R/find_variance_in_free.R
+++ R/find_variance_in_free.R
@@ -40,3 +40,22 @@ find_variance_in_free <- function(sem_out) {
     id_var <- i_id[i_var]
     id_free %in% id_var
   }
+
+find_variance_in_free_lb <- function(sem_out, prop = .05, se_k = 3) {
+    if (!inherits(sem_out, "lavaan")) {
+        stop("sem_out is not a supported object.")
+      }
+    ptable <- lavaan::parameterTable(sem_out)
+    # Do not check for
+    i_id <- ptable$id
+    i_free <- ptable$free > 0
+    id_free <- i_id[find_free(sem_out)]
+    i_var <- (ptable$lhs == ptable$rhs) & (ptable$op == "~~")
+    id_var <- i_id[i_var]
+    out_est <- ptable$est[id_var]
+    out_se <- ptable$se[id_var]
+    out_min <- out_est * prop
+    out_se3 <- out_est - out_se * se_k
+    out <- ifelse(out_se3 < 0, out_min, out_se3)
+    out
+  }
