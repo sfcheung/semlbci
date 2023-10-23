bb75b1223535cb166511c40804976b8448941a25
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-10-23 09:58:57 +0800

    WIP

---
+++ tests/testthat/test-semlbci_print_wn_mg_ustd_sem_user_eq.R
@@ -0,0 +1,37 @@
+skip("WIP")
+skip_on_cran()
+library(testthat)
+library(semlbci)
+
+
+# Fit the model
+
+library(lavaan)
+
+data(cfa_two_factors_mg)
+dat <- cfa_two_factors_mg
+mod <-
+"
+f1 =~ x1 + c(a1, a2)*x2 + c(c1, c2)*x3
+f2 =~ x4 + c(b1, b2)*x5 + c(d1, d2)*x6
+f1 ~ c(fr1, fr2)*f2
+ab := a1 * b2
+c1 == c2
+"
+fit <- lavaan::sem(mod, cfa_two_factors_mg, group = "gp")
+
+# Find the LBCIs
+
+pars <- c("c2 :=",
+          "f1 ~ f2",
+          "ab :=")
+pars_i <- syntax_to_i(pars, fit)
+system.time(
+    lbci_fit <- semlbci(fit,
+                        pars = pars,
+                        method = "wn",
+                        verbose = TRUE,
+                        opts = list(ftol_rel = 1e-6))
+  )
+
+lbci_fit
